c2088f97b30a6a3f84f01800612dad95e0794545
unknown <andresjose.210299@gmail.com>
2024-04-27 15:34:59 -0400

    agua caliente

--- src/app/components/agua-caliente/agua-caliente.component.ts
+++ src/app/components/agua-caliente/agua-caliente.component.ts
@@ -9,6 +9,7 @@ import { environment } from '../../../environments/environment.development';
 import { ApiService } from '../../services/api.service';
 import { Uf } from '../../interfaces/Iuf';
 import { CommonModule } from '@angular/common';
+import { CLIENT_RENEG_LIMIT } from 'node:tls';
 
 @Component({
   selector: 'app-agua-caliente',
@@ -20,31 +21,67 @@ import { CommonModule } from '@angular/common';
 export class AguaCalienteComponent {
   data: Uf[] = [];
   fecha: Date = new Date();
-  valor: number = 0;
+  valorUF: number = 0;
   ufContrato: number = 0.2;
+  precioMetroCubico: number =0;
+  lecturaInicial: number = 0;
+  lecturaFinal: number = 0;
+  consumoTotal: number = 0;
+  totalPago: number =0;
+  isChecked: boolean = false;
+  readonly:boolean = false
+
 
   form = new FormGroup({
     id: new FormControl(''),
-    ufContrato: new FormControl()
+    ufContrato: new FormControl(),
+    lecturaInicial: new FormControl(),
+    lecturaFinal: new FormControl(),
   });
 
   constructor(private ufService: ApiService) {}
 
   ngOnInit(): void {
     this.obtenerValorUF();
+    this.calcularPrecioMetroCubico();
+    this.modificarValorContrato()
   }
 
   obtenerValorUF() {
     this.ufService.obtenerValorUF().subscribe((res) => {
       res.UFs.map((uf) => {
-        this.valor = parseFloat(uf.Valor);
+        this.valorUF = parseFloat(uf.Valor.replace('.', ''));
         this.fecha = uf.Fecha;
       });
     });
   }
 
-  calcularAguaCaliente() {
+  calcularPrecioMetroCubico() {
     this.ufContrato = this.form.value.ufContrato
-    console.log(this.ufContrato);
+    this.precioMetroCubico = this.ufContrato * this.valorUF
+    this.calcularPagoTotal(this.precioMetroCubico,this.consumoTotal)
+  }
+  modificarValorContrato(){
+     this.readonly = !this.readonly
+     if(!this.isChecked){
+      this.ufContrato = 0.2
+      this.precioMetroCubico = this.ufContrato * this.valorUF
+      this.calcularPagoTotal(this.precioMetroCubico,this.consumoTotal)
+
+     }else{
+      this.ufContrato = this.form.value.ufContrato
+      this.precioMetroCubico = this.ufContrato * this.valorUF
+      this.calcularPagoTotal(this.precioMetroCubico,this.consumoTotal)
+     }
+
+  }
+  calcularConsumoAguaCaliente(){
+      this.lecturaInicial = this.form.value.lecturaInicial
+      this.lecturaFinal = this.form.value.lecturaFinal
+      this.consumoTotal= this.lecturaFinal - this.lecturaInicial
+      this.calcularPagoTotal(this.precioMetroCubico,this.consumoTotal)
+  }
+  calcularPagoTotal(precioMetroCubico: number, consumoTotal:number ){
+    this.totalPago= consumoTotal*precioMetroCubico
   }
 }
