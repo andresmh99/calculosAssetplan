2747e8b79892b711912ce4610c6386060a07f456
Jose Martinez <andresjose.210299@gmail.com>
2024-05-14 01:12:46 -0400

    menu responsive

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6V5PR7XD.mjs";import"./chunk-HSAEUPQ7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-EZ3SBEF3.mjs";import"./chunk-HSAEUPQ7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { afterRender, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+
+  constructor(){
+    afterRender(() =>{
+      const menu: HTMLElement | null = document.querySelector('#menu');
+
+    if (menu) {
+      menu.classList.toggle('hidden')
+    }
+    })
+  }
+
+  abrirMenu() {
+
+  }
+}
