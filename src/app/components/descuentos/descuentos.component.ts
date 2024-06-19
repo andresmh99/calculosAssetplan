8c2e0c399b3d24816fd4e973923f000ebca0faac
unknown <andresjose.210299@gmail.com>
2024-06-19 18:55:02 -0400

    Descuentos v3

--- src/app/components/descuentos/descuentos.component.ts
+++ src/app/components/descuentos/descuentos.component.ts
@@ -1,6 +1,6 @@
-import { CommonModule, CurrencyPipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule} from '@angular/common';
+import { Component } from '@angular/core';
+import {  FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-descuentos',
@@ -27,73 +27,79 @@ export class DescuentosComponent {
   form = new FormGroup({
     fechaInicio: new FormControl(),
     diasDescuento: new FormControl(),
-    tasaDescuento: new FormControl(),
+    tasaDescuento: new FormControl(this.tasaDescuento),
     canonMensual: new FormControl(),
   })
 
   ngOnInit(): void {
+    this.form.get('tasaDescuento')?.setValue(this.tasaDescuento); // Asegura que el form control tenga el valor inicial
   }
 
   calcularDescuentos() {
-    const valores = this.form.value;
-    this.tasaDescuento = valores.tasaDescuento
+    
+      const { fechaInicio, diasDescuento, canonMensual } = this.form.value;
+      if(fechaInicio){
+      let { tasaDescuento } = this.form.value;
   
-    // Convertir la fecha de inicio a un objeto Date
-    this.fechaInicio = new Date(valores.fechaInicio + 'T00:00:00');
-  
-    // Calcular la fecha final sumando los días de descuento a la fecha de inicio
-    const fin = new Date(this.fechaInicio);
-    fin.setDate(fin.getDate() + valores.diasDescuento);
-    this.fechaFinal = fin;
-  
-    // Inicializar variables
-    let fechaActual = new Date(this.fechaInicio);
-    this.descuentos = [];
-    this.totalDescuento = 0;
-  
-    // Iterar hasta llegar a la fecha final
-    while (fechaActual < fin) {
-      const year = fechaActual.getFullYear();
-      const month = fechaActual.getMonth();
-      const diasEnMes = new Date(year, month + 1, 0).getDate();
-      const diasRestantesEnMes = diasEnMes - fechaActual.getDate() + 1;
-  
-      // Calcular días a descontar
-      let diasDescontar = Math.min(diasRestantesEnMes, valores.diasDescuento);
-  
-      // Ajuste para contar correctamente el día inicial
-      if (fechaActual.getDate() === this.fechaInicio.getDate()) {
-        diasDescontar--; // Restar 1 día para incluir el día inicial
+      // Utiliza la tasa de descuento por defecto si no se ha especificado otra
+      if (tasaDescuento === null || tasaDescuento === undefined) {
+        tasaDescuento = this.tasaDescuento;
       }
+      // Convertir la fecha de inicio a un objeto Date
+      this.fechaInicio = new Date(`${fechaInicio}T00:00:00`);
   
-      const montoDescuento = diasDescontar * valores.canonMensual / diasEnMes * (this.tasaDescuento / 100);
-      this.totalDescuento += montoDescuento;
+      // Calcular la fecha final sumando los días de descuento a la fecha de inicio
+      this.fechaFinal = new Date(this.fechaInicio);
+      this.fechaFinal.setDate(this.fechaFinal.getDate() + diasDescuento);
   
-      // Buscar si ya existe un registro para el mes actual
-      const mesActual = fechaActual.toLocaleString('default', { month: 'long' });
-      const descuentoExistente = this.descuentos.find(d => d.mes === mesActual);
+      // Inicializar variables
+      let fechaActual = new Date(this.fechaInicio);
+      this.descuentos = [];
+      this.totalDescuento = 0;
+      let diasRestantes = diasDescuento;
   
-      if (descuentoExistente) {
-        // Si ya existe, sumar al registro existente
-        descuentoExistente.diasDescontados += diasDescontar;
-        descuentoExistente.montoDescuento += montoDescuento;
-      } else {
-        // Si no existe, crear un nuevo registro
-        this.descuentos.push({
-          mes: mesActual,
-          diasDescontados: diasDescontar,
-          montoDescuento: montoDescuento
-        });
+      // Iterar hasta llegar a la fecha final
+      while (diasRestantes > 0) {
+        const year = fechaActual.getFullYear();
+        const month = fechaActual.getMonth();
+        const diasEnMes = new Date(year, month + 1, 0).getDate();
+        const diasRestantesEnMes = diasEnMes - fechaActual.getDate() + 1;
+  
+        // Calcular días a descontar
+        const diasDescontar = Math.min(diasRestantesEnMes, diasRestantes);
+  
+        const montoDescuento = (diasDescontar * canonMensual / diasEnMes) * (tasaDescuento / 100);
+        this.totalDescuento += montoDescuento;
+  
+        // Buscar si ya existe un registro para el mes actual
+        const mesActual = fechaActual.toLocaleString('default', { month: 'long' });
+        const descuentoExistente = this.descuentos.find(d => d.mes === mesActual);
+  
+        if (descuentoExistente) {
+          // Si ya existe, sumar al registro existente
+          descuentoExistente.diasDescontados += diasDescontar;
+          descuentoExistente.montoDescuento += montoDescuento;
+        } else {
+          // Si no existe, crear un nuevo registro
+          this.descuentos.push({
+            mes: mesActual,
+            diasDescontados: diasDescontar,
+            montoDescuento
+          });
+        }
+  
+        // Avanzar la fecha actual al siguiente periodo
+        fechaActual.setDate(fechaActual.getDate() + diasDescontar);
+        diasRestantes -= diasDescontar; // Actualizar los días de descuento restantes
       }
-  
-      // Avanzar la fecha actual al siguiente periodo
-      fechaActual.setDate(fechaActual.getDate() + diasDescontar);
-      valores.diasDescuento -= diasDescontar; // Actualizar los días de descuento restantes
     }
-
   }
   
-
+  onInputChange() {
+    this.tasaDescuento = this.form.value.tasaDescuento ?? this.tasaDescuento;
+    this.mostrarValores();
+    this.calcularDescuentos();
+  }
 
 
   mostrarValores(){
